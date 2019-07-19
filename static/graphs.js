3aa79c7fe3a13ebfb744b5c02a298ce084baae0b
Shane Ding <shane200195@gmail.com>
2019-07-19 14:25:29 -0400

    Version with complete graph selection, and graph bug fixed

--- static/graphs.js
+++ static/graphs.js
@@ -1,18 +1,24 @@
 function graphs(choice, data, y_label){
     if (choice == 'ACTG Count') {
         var set = 'DNA_Count';
-        create_bar_graph(Object.keys(data[set]), Object.values(data[set]), y_label);
+        create_bar_graph('bar', Object.keys(data[set]), Object.values(data[set]), y_label);
     }
     if (choice == 'DNA Motif') {
         var set = 'DNA_motif_combo';
-        create_bar_graph(Object.keys(data[set]), Object.values(data[set]), y_label);
+        create_bar_graph('bar', Object.keys(data[set]), Object.values(data[set]), y_label);
+    }
+    if (choice == 'Symbol Array') {
+        var set = 'DNA_substirng_frequency';
+        create_bar_graph('line', Object.keys(data[set]), Object.values(data[set]), y_label);
     }
 }
 
-function create_bar_graph(graph_x_labels, graph_data, graph_y_label){
-    var ctx = document.getElementById('myChart').getContext('2d');
+function create_bar_graph(graph_type, graph_x_labels, graph_data, graph_y_label){
+    $('#myChart').remove();
+    $('.graph-container').append('<canvas id="myChart" width="400" height="200"></canvas>');
+    var ctx = document.querySelector('#myChart').getContext('2d');
     var myChart = new Chart(ctx, {
-        type: 'bar',
+        type: graph_type,
         data: {
             labels: graph_x_labels,
             datasets: [{
@@ -25,7 +31,7 @@ function create_bar_graph(graph_x_labels, graph_data, graph_y_label){
             scales: {
                 yAxes: [{
                     ticks: {
-                        beginAtZero: true
+                        beginAtZero: false
                     }
                 }]
             }
